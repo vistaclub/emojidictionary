c16dbaf0d4c51660aa2db2e1c56f68fda9badd8d
Jason Wong <jawong71@gmail.com>
2018-06-07 08:23:50 -0700

    Added emoji graphics to code for the row/cell

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -8,11 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var emojitableview: UITableView!
+    
+    var emojis = ["😝","🐶","😜","🤡","👻","😡","😎"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        emojitableview.dataSource = self
+        emojitableview.delegate = self
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojis.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print(indexPath.row)
+        let cell = UITableViewCell()
+        cell.textLabel?.text = emojis[indexPath.row]
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
